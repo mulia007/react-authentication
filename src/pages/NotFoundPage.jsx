df44982e7043333fce683acc7f1308d971b58833
mulia007 <116814230+mulia007@users.noreply.github.com>
2022-12-29 23:32:27 +0700

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
   return (
     <div>
-      <h1>NotFoundPage</h1>
+      <h1>NotFoundPages</h1>
       <Link to="/">back to Home</Link>
     </div>
   );
